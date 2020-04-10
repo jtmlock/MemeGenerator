d9fbbab77e71148ab00faf6be983256cc7d5f981
Adam Smith <jtmlock@gmail.com>
2020-04-09 17:12:27 -0700

    trying to push and pull - cloned repository name into XCode- couldn't authenticate- now can't pull

---
+++ Pods/Target Support Files/Pods-Meme Generator-Meme GeneratorUITests/Pods-Meme Generator-Meme GeneratorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Meme_Generator_Meme_GeneratorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Meme_Generator_Meme_GeneratorUITests
+@end
---
+++ Pods/Target Support Files/Pods-Meme Generator/Pods-Meme Generator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Meme_Generator : NSObject
+@end
+@implementation PodsDummy_Pods_Meme_Generator
+@end
