b5e3657da18870b33981e0bbc1274a2d13bfaf7d
Adam Smith <jtmlock@gmail.com>
2020-04-19 02:25:36 -0700

    added Album View controller with modal presentation, re-arranged login screen Image, user log in labels, and password labels

---
+++ Meme Generator/AlbumViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumViewController.swift
+//  Meme Generator
+//
+//  Created by Mac on 4/19/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
