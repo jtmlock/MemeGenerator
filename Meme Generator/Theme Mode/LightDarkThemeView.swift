0826f20d3604ae6525745d2c005b1299cb27b59f
Raul Rivero Rubio <morosoft_dev@yahoo.com>
2020-04-21 06:43:49 -0700

    - Restructuring the UISwitch control and UIImageView for the theme mode to use (Reusable UIView)

---
+++ Meme Generator/Theme Mode/LightDarkThemeView.swift
@@ -0,0 +1,21 @@
+//
+//  LightDarkThemeView.swift
+//  Meme Generator
+//
+//  Created by vagrant on 4/21/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class LightDarkThemeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
