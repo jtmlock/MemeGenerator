b554a838868301f1b0646349430a8bc13ca0add9
Adam Smith <jtmlock@gmail.com>
2020-04-20 21:00:49 -0700

    Centered items on load screen, changed title of Feed View Controller to Home, added sign out buttons to feed view controller and settings scene, added title to settings scene, added title to settings scene

--- Meme Generator/LoginViewController.swift
+++ Meme Generator/LoginViewController.swift
@@ -11,18 +11,14 @@ import Parse
 
 class LoginViewController: UIViewController {
     
-
-
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
     
     @IBAction func onSignIn(_ sender: Any) {
         let username = usernameField.text!
