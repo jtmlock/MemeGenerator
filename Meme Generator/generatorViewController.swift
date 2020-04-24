08f23a5f2d809d8a60bcbbac43dd08b4d69362ea
Adam Smith <jtmlock@gmail.com>
2020-04-23 19:29:26 -0700

    -Persistant Log in -User is able to switch between camera and album -User is able to logout

--- Meme Generator/generatorViewController.swift
+++ Meme Generator/generatorViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 class generatorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
     
 
     override func viewDidLoad() {
@@ -22,12 +26,46 @@ class generatorViewController: UIViewController, UITableViewDelegate, UITableVie
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground{ (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+                
+            }
+        }
+            
+            
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return posts.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as? String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        return cell
     }
     
     @IBAction func onBackFromGeneratorToHome(_ sender: Any) {
