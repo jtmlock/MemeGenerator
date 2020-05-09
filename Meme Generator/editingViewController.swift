d6794205331fb12b41eeb2306bd116850b13b07a
Adam Smith <jtmlock@gmail.com>
2020-05-08 20:06:51 -0700

    added text to draw on editor page

--- Meme Generator/editingViewController.swift
+++ Meme Generator/editingViewController.swift
@@ -37,6 +37,40 @@ class editingViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func textToImage(drawText: NSString, inImage: UIImage, atPoint: CGPoint) -> UIImage{
+
+    // Setup the font specific variables
+    let textColor = UIColor.white
+    let textFont = UIFont(name: "Helvetica Bold", size: 12)!
+
+    // Setup the image context using the passed image
+    let scale = UIScreen.main.scale
+    UIGraphicsBeginImageContextWithOptions(inImage.size, false, scale)
+
+    // Setup the font attributes that will be later used to dictate how the text should be drawn
+    let textFontAttributes = [
+        NSAttributedString.Key.font: textFont,
+        NSAttributedString.Key.foregroundColor: textColor,
+    ]
+
+    // Put the image into a rectangle as large as the original image
+    inImage.draw(in: CGRect(x: 0, y: 0, width: inImage.size.width, height: inImage.size.height))
+
+    // Create a point within the space that is as bit as the image
+    let rect = CGRect(x: atPoint.x, y: atPoint.y, width: inImage.size.width, height: inImage.size.height)
+
+    // Draw the text into an image
+    drawText.draw(in: rect, withAttributes: textFontAttributes)
+
+    // Create a new image out of the images we have created
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+
+    // End the context now that we have the image we need
+    UIGraphicsEndImageContext()
+
+    //Pass the image back up to the caller
+    return newImage!
+    }
 
     /*
     // MARK: - Navigation
