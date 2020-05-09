0d7c40aeb3edce8aef992d0f564bc99ca5978d0a
Adam Smith <jtmlock@gmail.com>
2020-05-09 03:22:35 -0700

    rearranged post area and text field as well as titles on views

--- Meme Generator/editingViewController.swift
+++ Meme Generator/editingViewController.swift
@@ -37,26 +37,29 @@ class editingViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
-        let textColor = UIColor.white
-        let textFont = UIFont(name: "Helvetica Bold", size: 12)!
+    @IBAction func saveMeme(_ sender: Any) {
+    
+        func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
+            let textColor = UIColor.white
+            let textFont = UIFont(name: "Helvetica Bold", size: 12)!
 
-        let scale = UIScreen.main.scale
-        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+            let scale = UIScreen.main.scale
+            UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
 
-        let textFontAttributes = [
-            NSAttributedString.Key.font: textFont,
-            NSAttributedString.Key.foregroundColor: textColor,
-            ] as [NSAttributedString.Key : Any]
-        image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
+            let textFontAttributes = [
+                NSAttributedString.Key.font: textFont,
+                NSAttributedString.Key.foregroundColor: textColor,
+                ] as [NSAttributedString.Key : Any]
+            image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
 
-        let rect = CGRect(origin: point, size: image.size)
-        text.draw(in: rect, withAttributes: textFontAttributes)
+            let rect = CGRect(origin: point, size: image.size)
+            text.draw(in: rect, withAttributes: textFontAttributes)
 
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
+            let newImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
 
-        return newImage!
+            return newImage!
+        }
     }
 
     /*
